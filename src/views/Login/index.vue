5bd9d51e6cb3a1974d7bd6f930e4f0bd95b91645
reamey <17318077256@163.com>
2019-11-06 15:28:23 +0800

    重置样式 加入element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    我是登录页
+      <el-button type="primary">主要按钮</el-button>
+
+  </div>
+</template>
+<script>
+export default {
+  name:'login'
+}
+</script>
+<style lang="scss" scoped>
+  #login{
+    height: 100vh;
+    background: #344a5f
+  }
+</style>
